d63a1633da1d2367e7163147cd384c31b43ffa7a
Gabriela C Serafim <gabrielacamiloserafim@gmail.com>
2022-04-20 00:57:11 -0300

    busy loader e mensagem de sucesso

--- src/app/gastos/adicionar/adicionar.page.ts
+++ src/app/gastos/adicionar/adicionar.page.ts
@@ -2,6 +2,7 @@ import { Component, OnDestroy, OnInit } from '@angular/core';
 import { MessageService } from '../../services/message.service';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { ActivatedRoute, Router } from '@angular/router';
+import { finalize } from 'rxjs/operators';
 import {
   ViewDidEnter,
   ViewDidLeave,
@@ -17,43 +18,48 @@ import { Gasto, Mes, MetodoPagamento } from '../gastos.model';
   styleUrls: ['./adicionar.page.scss'],
 })
 export class AdicionarPage implements
-    OnInit,
-    OnDestroy,
-    ViewWillEnter,
-    ViewDidEnter,
-    ViewWillLeave,
-    ViewDidLeave
+OnInit,
+OnDestroy,
+ViewWillEnter,
+ViewDidEnter,
+ViewWillLeave,
+ViewDidLeave
 {
   form: FormGroup;
-
+  loading = false;
+  
+  
   constructor(
     private formBuilder: FormBuilder,
     private gastosService: GastosService,
     private router: Router,
     private activatedRoute: ActivatedRoute,
     private messageService: MessageService
-  ) {}
-
-  ngOnInit() {
-    console.log('AdicionarPage ngOnInit');
-    this.form = this.formBuilder.group({
-      id:[''],
-      nome: ['', Validators.required],
-      metod_pag: ['Cartão de Crédito',Validators.required],
-      valor: [0.1, Validators.required],
-      data: ['', Validators.required],
-      mes: ['Novembro', Validators.required],
-    });
-
-    const id = +this.activatedRoute.snapshot.params.id;    
-    if(id){
-      this.findById(id);
+    ) {}
+    
+    ngOnInit() {
+      console.log('AdicionarPage ngOnInit');
+      this.form = this.formBuilder.group({
+        id:[''],
+        nome: ['', Validators.required],
+        metod_pag: ['Cartão de Crédito',Validators.required],
+        valor: [0.1, Validators.required],
+        data: ['', Validators.required],
+        mes: ['Novembro', Validators.required],
+      });
+      
+      const id = +this.activatedRoute.snapshot.params.id;    
+      if(id){
+        this.findById(id);
+      }
     }
-  }
-
-  findById(id: number){
-    this.gastosService.findById(id).subscribe(
-      (gasto) => {
+    
+    findById(id: number){
+      this.loading = true;
+      this.gastosService.findById(id).pipe(finalize(() => {
+        this.loading = false;
+      })
+      ).subscribe((gasto) => {
         if (gasto) {
           this.form.patchValue({
             ...gasto,
@@ -61,35 +67,45 @@ export class AdicionarPage implements
         }
       },
       () => this.messageService.error(`Erro ao buscar o gasto com o código <b>${id}</b>`,() => this.findById(id))
-    );
-  }
-  ionViewWillEnter(): void {
-    console.log('AdicionarPage ionViewWillEnter');
-  }
-
-  ionViewDidEnter(): void {
-    console.log('AdicionarPage ionViewDidEnter');
-  }
-
-  ionViewWillLeave(): void {
-    console.log('AdicionarPage ionViewWillLeave');
-  }
-
-  ionViewDidLeave(): void {
-    console.log('AdicionarPage ionViewDidLeave');
-  }
-
-  ngOnDestroy(): void {
-    console.log('AdicionarPage ngOnDestroy');
-  }
-  salvar() {
-    const { nome } = this.form.value;
-    this.gastosService.save(this.form.value).subscribe(
-      () => this.router.navigate(['tabs/inicio']),
-      () =>
-      this.messageService.error(`Erro ao salvar o novo gasto <b>${nome}</b>`, () =>
+      );
+    }
+    ionViewWillEnter(): void {
+      console.log('AdicionarPage ionViewWillEnter');
+    }
+    
+    ionViewDidEnter(): void {
+      console.log('AdicionarPage ionViewDidEnter');
+    }
+    
+    ionViewWillLeave(): void {
+      console.log('AdicionarPage ionViewWillLeave');
+    }
+    
+    ionViewDidLeave(): void {
+      console.log('AdicionarPage ionViewDidLeave');
+    }
+    
+    ngOnDestroy(): void {
+      console.log('AdicionarPage ngOnDestroy');
+    }
+    salvar() {
+      const { nome } = this.form.value;
+      this.loading = true;
+      
+      this.gastosService.save(this.form.value).pipe(finalize(() => {
+        this.loading = false;
+      })
+      )      
+      .subscribe(
+        () => {
+          this.messageService.success(`O gasto ${nome} foi salvo sucesso!`);
+          this.router.navigate(['tabs/inicio']);
+        },
+        () =>
+        this.messageService.error(`Erro ao salvar o novo gasto <b>${nome}</b>`, () =>
         this.salvar()
-      )
-    ); 
-  }
-}
+        )
+        ); 
+      }
+    }
+    
--- src/app/gastos/inicio/inicio.page.ts
+++ src/app/gastos/inicio/inicio.page.ts
@@ -1,15 +1,16 @@
 import { Component,OnDestroy, OnInit } from '@angular/core';
 import {
-AlertController, 
-ToastController,
-ViewDidEnter, 
-ViewDidLeave, 
-ViewWillEnter,
-ViewWillLeave,
+  AlertController, 
+  ToastController,
+  ViewDidEnter, 
+  ViewDidLeave, 
+  ViewWillEnter,
+  ViewWillLeave,
 } from '@ionic/angular';
 import { Gasto } from '../gastos.model';
 import { MessageService } from 'src/app/services/message.service';
 import { GastosService } from '../gastos.service';
+import { finalize } from 'rxjs/operators';
 
 @Component({
   selector: 'app-inicio',
@@ -23,81 +24,86 @@ ViewWillEnter,
 ViewDidEnter,
 ViewWillLeave,
 ViewDidLeave {
-
+  
   gastos: Gasto[];
   loading = false;
-
+  
   constructor(
     private alertController: AlertController,
     private gastosService: GastosService,
     private messageService: MessageService
-  ) {
-    this.gastos = [];
-  }
-  
-  ngOnInit() {
-    console.log('InicioPage ngOnInit');
-  }
-
-  ionViewWillEnter(): void {
-    this.listGastos();
-    console.log('InicioPage ionViewWillEnter');
-  }
-
-  ionViewDidEnter(): void {
-    console.log('InicioPage ionViewDidEnter');
-  }
-
-  ionViewWillLeave(): void {
-    console.log('InicioPage ionViewWillLeave');
-  }
-
-  ionViewDidLeave(): void {
-    console.log('InicioPage ionViewDidLeave');
-  }
-
-  ngOnDestroy(): void {
-    console.log('InicioPage ngOnDestroy');
-  }
-
-  listGastos(){
-    this.loading = true;
-    this.gastosService.getGastos().subscribe(
-      (gastos) => {
-        this.gastos = gastos
+    ) {
+      this.gastos = [];
+    }
+    
+    ngOnInit() {
+      console.log('InicioPage ngOnInit');
+    }
+    
+    ionViewWillEnter(): void {
+      this.listGastos();
+      console.log('InicioPage ionViewWillEnter');
+    }
+    
+    ionViewDidEnter(): void {
+      console.log('InicioPage ionViewDidEnter');
+    }
+    
+    ionViewWillLeave(): void {
+      console.log('InicioPage ionViewWillLeave');
+    }
+    
+    ionViewDidLeave(): void {
+      console.log('InicioPage ionViewDidLeave');
+    }
+    
+    ngOnDestroy(): void {
+      console.log('InicioPage ngOnDestroy');
+    }
+    
+    listGastos(){
+      this.loading = true;
+      this.gastosService.getGastos().pipe(finalize(() => {
         this.loading = false;
-      },
-      () => {
-        this.messageService.error('Erro ao buscar a lista de gastos', () => this.listGastos())
-        this.loading = false;
-      }
-    );
-  }  
-
-  confirmRemove(gasto: Gasto) {
-    this.alertController
-      .create({
-        header: 'Exclusão',
-        message: `Você deseja excluir o gasto <b>${gasto.nome}</b>?`,
-        buttons: [
-          {
-            text: 'Sim',
-            handler: () => this.remove(gasto),
-          },
-          {
-            text: 'Não',
-          },
-        ],
       })
-      .then((alert) => alert.present());
-  }
-
-  remove(gasto: Gasto) {
-    this.gastosService.remove(gasto.id)
+      )
       .subscribe(
-      () => {
-        this.listGastos();
-      },
-      () => this.messageService.error('Erro ao excluir o gasto', () => this.remove(gasto)));
-  }   
-}
+        (gastos) => (this.gastos = gastos),
+        () => 
+        this.messageService.error('Erro ao buscar a lista de gastos', () => this.listGastos()
+        )
+        );
+      }  
+      
+      confirmRemove(gasto: Gasto) {
+        this.alertController
+        .create({
+          header: 'Exclusão',
+          message: `Você deseja excluir o gasto <b>${gasto.nome}</b>?`,
+          buttons: [
+            {
+              text: 'Sim',
+              handler: () => this.remove(gasto),
+            },
+            {
+              text: 'Não',
+            },
+          ],
+        })
+        .then((alert) => alert.present());
+      }
+      
+      remove(gasto: Gasto) {
+        this.loading = true;
+        this.gastosService.remove(gasto.id).subscribe(
+          () => {
+            this.messageService.success(`O gasto ${gasto.nome} foi excluído com sucesso!`);
+            this.listGastos();
+          },
+          () => { this.messageService.error('Erro ao excluir o gasto', () => this.remove(gasto));
+          this.loading = false;
+        }   
+        );
+      }
+    }
+    
